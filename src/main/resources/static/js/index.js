9717c07723e8e1eaf3cb4fbf4673a17c3fb414b9
ecoutoo <viccontecouto@gmail.com>
2021-04-07 11:41:17 +0100

    CHOON-9 #Template frontend page done

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,21 @@
+'use strict'
+
+let username = "Username Here";
+
+function displayLoginDetails(data){
+    /* Displays (if valid) the username, then either login or logout button*/
+    console.log("Displaying Login Detail")
+    let navbar = document.getElementById('rightNavbar');
+    let logLink = document.getElementById('logNavLink');
+    let finalItem = document.getElementById('finalNavListItem');
+
+    let li = document.createElement('li');
+    li.innerHTML = data;
+    li.className = 'username';
+    /* navbar.appendChild(li); */
+    navbar.insertBefore(li,finalItem);
+
+    logLink.innerText = "Logout";
+}
+
+displayLoginDetails(username); 
