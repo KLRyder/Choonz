6ba55a1f4068c802fe1e661c459806da2283e50e
Tom Hoey <thoey@academytrainee.com>
2021-04-07 13:09:57 +0100

    CHOON-120 #comment Added Model and DTO unit tests for Playlist.

--- src/test/java/com/qa/choonz/rest/dto/playlistDTOUnitTest.java
+++ src/test/java/com/qa/choonz/rest/dto/playlistDTOUnitTest.java
@@ -1,5 +1,30 @@
 package com.qa.choonz.rest.dto;
 
+import org.junit.jupiter.api.Test;
+
+import com.qa.choonz.persistence.domain.Playlist;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+
 public class playlistDTOUnitTest {
+	
+	private final AlbumDTO album = new AlbumDTO();
+	private final ArtistDTO artist = new ArtistDTO();
+	private final GenreDTO genre = new GenreDTO();
+	
+	@Test
+	void testEquals() {
+		EqualsVerifier.simple().forClass(PlaylistDTO.class)
+			.withPrefabValues(TrackDTO.class, 
+					new TrackDTO(1, "name1", album, null, 300, "lyrics", artist, genre), 
+					new TrackDTO(2, "name2", album, null, 300, "lyrics", artist, genre))
+			.verify();
+	}
+	
+	@Test
+	void contructorTest() {
+		PlaylistDTO newPlaylistModel = new PlaylistDTO(1, "name", "description", "artwork");
+		newPlaylistModel.toString();
+	}
 
 }
