41d25b40d2edfb6b8085cba808de5579daf6606d
ecoutoo <viccontecouto@gmail.com>
2021-04-08 15:44:02 +0100

    CHOON-13 #Track page information load

---
+++ src/main/resources/static/js/fillTrackPage.js
@@ -0,0 +1,40 @@
+'use strict'
+const apiURL = 'http://localhost:8082/'
+
+let fill = (trackJSON) => {
+
+    console.log("Try to fill in information")
+    let trackNameText = document.querySelector('#trackName');
+    trackNameText.innerHTML = trackNameText.innerHTML.replace("TRACK NAME", trackJSON.name)
+
+    let basicTrackInfo = document.getElementById("trackInfoRow");
+    basicTrackInfo.innerHTML = basicTrackInfo.innerHTML.replace("_ARTIST-ID", trackJSON.artist.id)
+    .replace("_ARTIST-NAME", trackJSON.artist.name)
+    .replace("_ALBUM-ID", trackJSON.album.id)
+    .replace("_ALBUM-NAME", trackJSON.album.name)
+    // .replace("_GENRE-ID", trackJSON.genre.id)
+    // .replace("_GENRE-NAME", trackJSON.genre.name)
+
+    let lyricText = document.getElementById("lyricsBox");
+    let fullLyrics = trackJSON.lyrics;
+    let splitLyrics = fullLyrics.split(".");
+    linebreak = document.createElement("br");
+    for (let i = 0; i < splitLyrics.length; i++) {
+        document.getElementById('lyricsBox').append(splitLyrics[i] + ".");
+        document.getElementById('lyricsBox').append(linebreak);
+    }
+}
+
+let trackInfoFill = (trackId) => {
+    console.log("try to read track by Id")
+    fetch(apiURL + 'tracks/read/' + trackId).then(res => res.json())
+        .then((data) => {
+            fill(data);
+        })
+        .catch((error) => console.error(`Request failed ${error}`))
+}
+
+console.log("try to run script")
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+trackInfoFill(urlParams.get("track_id"));
