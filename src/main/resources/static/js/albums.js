8635476f4e21fb227f22ef0f202e04f877a94535
ecoutoo <viccontecouto@gmail.com>
2021-04-12 16:20:19 +0100

    Temp modal js

--- src/main/resources/static/js/albums.js
+++ src/main/resources/static/js/albums.js
@@ -1,19 +1,72 @@
-let deleteAlbumModal = document.getElementById("deleteAlbumModal");
-
-function displayDeleteAlbumModal(){
-    // Displays the confirmation modal
-    console.log("Displaying delete album confirm modal")
-    deleteAlbumModal.style.display = "block";
-}
-
-function closeDeleteAlbumModal(){
-    // Displays the confirmation modal
-    console.log("Closing delete album confirm modal")
-    deleteAlbumModal.style.display = "none";
-}
-
-window.onclick = function(event) {
-    if (event.target == deleteAlbumModal) {
-        deleteAlbumModal.style.display = "none";
+function deleteAlbum(){
+    if (dataType.value == "listdata") {
+        fetch("http://localhost:8080/todolist/" + id.value, {
+            method: 'delete'
+        }).then(res => {
+            if (res.status === 200) {
+                console.info(id.value + " deleted")
+                querySuccess();
+                return;
+            } else {
+                console.error(`Request failed ${res.body}`)
+            }
+        }).catch((error) => console.error(`Request failed ${error}`))
     }
-  }
+    else if (dataType.value == "itemdata"){
+        fetch("http://localhost:8080/item/" + id.value, {
+            method: 'delete'
+        }).then(res => {
+            if (res.status === 200) {
+                console.info(id.value + " deleted")
+                querySuccess();
+                return;
+            } else {
+                console.error(`Request failed ${res.body}`)
+            }
+        }).catch((error) => console.error(`Request failed ${error}`))
+    }
+    queryFailure();
+}
+
+function updateAlbum(){
+    clearInfo();
+    if (dataType.value == "listdata") {
+        fetch("http://localhost:8080/todolist/" + id.value, {
+            method: 'put',
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({
+                "title": textData.value,
+                "completed": document.getElementById("myCheck").checked
+            })
+        }).then(res => res.json())
+            .then((data) => {
+                displayList(data);
+                querySuccess();
+                return;
+            })
+            .catch((error) => console.error(`Request failed ${error}`))
+    }
+    else if (dataType.value == "itemdata"){
+        fetch("http://localhost:8080/item/" + id.value, {
+            method: 'put',
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({
+                "textBody": textData.value,
+                "taskDone": document.getElementById("myCheck").checked,
+                "toDoList": {
+                    "listId": listId.value
+                }
+            })
+        }).then(res => res.json())
+            .then((data) => {
+                querySuccess();
+                return;
+            })
+            .catch((error) => console.error(`Request failed ${error}`))
+    }
+    queryFailure();
+}
