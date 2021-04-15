115eb742f85f7634e3f00a7776e516a1cddfc68e
KLRyder <KLRyder@users.noreply.github.com>
2021-04-15 13:41:59 +0100

    CHOON-39 genre page improvements

--- src/main/resources/static/js/genres.js
+++ src/main/resources/static/js/genres.js
@@ -1,43 +1,44 @@
-function deleteGenre(){
+function deleteGenre() {
     console.info("Deleting genre")
 
     fetch("http://localhost:8082/genres/delete/" + urlParams.get("genre_id"), {
         method: 'delete'
     }).then(res => {
-        if (res.status === 200) {
+        if (res.status >= 200 && res.status <= 300) {
             console.info("Deleted successfully")
-            return;
+            window.location.href = "/";
         } else {
             console.error(`Request failed ${res.body}`)
         }
     }).catch((error) => console.error(`Request failed ${error}`))
 }
 
-function updateGenre(){
+function updateGenre() {
     console.info("Updating genre")
 
     let genreName = document.querySelector('#update-genre-name').value;
+    let genreDesc = document.querySelector('#update-genre-description').value;
 
     fetch("http://localhost:8082/genres/update/" + urlParams.get("genre_id"), {
-        method: 'put',
+        method: 'post',
         headers: {
             "Content-type": "application/json"
         },
         body: JSON.stringify({
-            "name": genreName
+            "name": genreName,
+            "description": genreDesc
         })
     }).then(res => res.json())
         .then((data) => {
-            console.info("Updated")
-            return;
+            fill(data, false)
         })
-         .catch((error) => console.error(`Request failed ${error}`))
+        .catch((error) => console.error(`Request failed ${error}`))
 }
 
-let fill = (genreJSON) => {
+let fill = (genreJSON, fillArtist) => {
 
     let genreNameText = document.querySelector('#genreName');
-    genreNameText.innerHTML = genreNameText.innerHTML.replace("GENRE NAME", genreJSON.name)
+    genreNameText.innerHTML = genreJSON.name;
 
     document.getElementById("genreDesc").innerHTML = genreJSON.description;
 
@@ -48,8 +49,10 @@ let fill = (genreJSON) => {
     }
     */
 
-    for (let i =0;i<genreJSON.albums.length;i++) {
-        displayAlbum(genreJSON.albums[i]);
+    if (fillArtist) {
+        for (let i = 0; i < genreJSON.albums.length; i++) {
+            displayAlbum(genreJSON.albums[i]);
+        }
     }
 
 }
@@ -57,7 +60,7 @@ let fill = (genreJSON) => {
 let genreInfoFill = (genreId) => {
     fetch(apiURL + 'genres/read/' + genreId).then(res => res.json())
         .then((data) => {
-            fill(data);
+            fill(data, true);
         })
         .catch((error) => console.error(`Request failed ${error}`))
 }
